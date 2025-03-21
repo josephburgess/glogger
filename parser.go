79a4665b8da033d361146e28a3dd8eec1f55bb93
josephburgess <josephburgess@gmail.com>
2025-03-21 23:08:34 +0000

    dont dupe title

--- parser.go
+++ parser.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"html/template"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/parser"
 )
 
 // parse md with goldmark
@@ -16,31 +19,75 @@ func parsePost(filename string) (Post, error) {
 		return Post{}, err
 	}
 
-	// default untitled post unless # found in a line
-	lines := strings.Split(string(content), "\n")
+	rawContent := string(content)
+	lines := strings.Split(rawContent, "\n")
+	
 	title := "Untitled Post"
-	for _, line := range lines {
+	var publishDate time.Time
+	
+	// find title
+	titleLine := -1
+	for i, line := range lines {
 		if strings.HasPrefix(line, "# ") {
 			title = strings.TrimPrefix(line, "# ")
+			titleLine = i
 			break
 		}
 	}
 
-	info, err := os.Stat(filename)
-	if err != nil {
-		return Post{}, err
+	// check for date in YYYY-MM-DD in first 3 lines
+	datePattern := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}`)
+	dateLine := -1
+	for i, line := range lines[:Min(3, len(lines))] {
+		if datePattern.MatchString(line) {
+			if date, err := time.Parse("2006-01-02", strings.TrimSpace(line)); err == nil {
+				publishDate = date
+				dateLine = i
+				break
+			}
+		}
 	}
 
-	md := goldmark.New()
+	// else use file modified date
+	if publishDate.IsZero() {
+		info, err := os.Stat(filename)
+		if err != nil {
+			return Post{}, err
+		}
+		publishDate = info.ModTime()
+	}
+
+	// remove date/title 
+	contentLines := make([]string, 0, len(lines))
+	for i, line := range lines {
+		if i != dateLine && i != titleLine {
+			contentLines = append(contentLines, line)
+		}
+	}
+	
+	filteredContent := strings.Join(contentLines, "\n")
+
+	md := goldmark.New(
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+	)
 	var buf bytes.Buffer
-	if err := md.Convert(content, &buf); err != nil {
+	if err := md.Convert([]byte(filteredContent), &buf); err != nil {
 		return Post{}, err
 	}
 
 	return Post{
 		Title:       title,
 		Content:     template.HTML(buf.String()),
-		RawContent:  string(content),
-		PublishDate: info.ModTime(),
+		RawContent:  rawContent,
+		PublishDate: publishDate,
 	}, nil
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
